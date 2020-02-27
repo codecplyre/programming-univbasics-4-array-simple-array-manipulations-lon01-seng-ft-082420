eaa7027cb633f229f0a62018a7c6d2450588f692
Ricky Adriell Cultura <pansoy.rickyadriell26457@gmail.com>
2020-02-27 19:12:22 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,35 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array)
+  abc = []
+  a = array.pop()
+  b = array.pop()
+  abc << b << a
+  abc
+end
+
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
+  abc = []
+  a = array.shift()
+  b = array.shift()
+  abc << a << b
+  abc
+end
+
+def using_concat(array, element)
+  array.concat(element)
+end
